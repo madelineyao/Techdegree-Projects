7ddd1f1fd5c34fc17e1ce3d240c71a52c936e5db
madelineyao <yao.madeline@gmail.com>
2018-08-14 14:04:50 -0700

    modify job role section and set focus on the first field set

---
+++ full-stack-project-3/js/script.js
@@ -0,0 +1,7 @@
+//set focus on the first text field in the text form 
+const first_textfield = document.querySelectorAll('fieldset')[0]; 
+first_textfield.focus(); 
+
+//add a text field for the 'Other' option in 'Job Role'
+const other_job_option = document.querySelector('#title').getElementsByTagName('option')[5]; 
+ 
