7480b1c9e3b4a1781b771bb062625a85adc576bd
madelineyao <yao.madeline@gmail.com>
2018-08-21 16:37:58 -0700

    create a new project for tic-tac toe and let the total payment show up in the form when choosing the events

--- full-stack-project-3/js/script.js
+++ full-stack-project-3/js/script.js
@@ -12,8 +12,9 @@ const theme_selection = document.getElementById('design');
 const color_menu = document.getElementById('color'); 
 
 theme_selection.addEventListener('click', (e)=>{
-    const colors = color_menu.children; 
-    if(e.target.textContent === 'Theme - JS Puns'){
+    const colors = color_menu.children;
+    if(e.target.tagName==='option'){
+        if(e.target.textContent === 'Theme - JS Puns'){
         for(let i = 0; i < colors.length; i += 1){
              if(colors.indexOf(colors[i])=== 3|4|5){
                  colors.removeChild(colors[i]); 
@@ -27,6 +28,71 @@ theme_selection.addEventListener('click', (e)=>{
             }
         }
     }
+    }
 }); 
- 
+
+
+//event selection: if there is one event checked that conflicted with another, disable the latter event checkbox
+const event_selection = document.getElementsByClassName('activities')[0]; 
+const events = event_selection.querySelectorAll('label'); 
+const div_for_total = document.createElement('div'); 
+const totalinfo = document.createElement('p'); 
+
+//checked the checkbox --> figure out the schedule-conflicting events --> disabled those events' checkboxes 
+//--> color the label ---> creat a div under the checkbox --> put dollar in the div and display 
+event_selection.addEventListener('change', (e) =>{
+    const checkbox = e.target; 
+
+    let totalPayment = 0; 
+    const div_for_total = document.createElement('div'); 
+    const totalinfo = document.createElement('p'); 
+   
+    div_for_total.appendChild(totalinfo); 
+    event_selection.appendChild(div_for_total); 
+    div_for_total.style.display = 'none'; 
+    if(checkbox.type==='checkbox' ){
+        if(checkbox.checked){
+        switch(checkbox.parentNode.name){
+            case "all": 
+                totalPayment = totalPayment + 200; 
+                break; 
+            case "js-frameworks": 
+                events[3].childNodes[0].disabled = true; 
+                events[5].childNodes[0].disabled = true;
+                totalPayment = totalPayment + 100;
+                break; 
+            case "js-libs": 
+                events[4].childNodes[0].disabled = true; 
+                events[6].childNodes[0].disabled = true; 
+                totalPayment += 100; 
+                break; 
+                
+            case "express": 
+                events[1].childNodes[0].disabled = true; 
+                events[5].childNodes[0].disabled = true; 
+                totalPayment += 100; 
+                break; 
+               
+            case "node": 
+                events[2].childNodes[0].disabled = true; 
+                events[6].childNodes[0].disabled = true; 
+                totalPayment += 100; 
+                break; 
+            case "build-tools": 
+                events[1].childNodes[0].disabled = true; 
+                events[3].childNodes[0].disabled = true; 
+                totalPayment += 100; 
+                break; 
+            case "npm": 
+                events[2].childNodes[0].disabled = true; 
+                events[4].childNodes[0].disabled = true; 
+                totalPayment += 100; 
+                break; 
+               
+               }
+            totalinfo.textContent = 'Total: '+ totalPayment; 
+            div_for_total.style.display = 'none'; 
+        } 
+    }
+}); 
 
